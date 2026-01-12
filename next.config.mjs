5d0941daf0e9db645dea3e17553e26eb0b72613c
Leandro <kukiurie333@gmail.com>
2026-01-11 21:42:32 -0300

    Fix: configuracion standalone y limpieza

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
+  // Esto ayuda a Vercel a forzar la salida correcta
+  output: 'standalone', 
 }
 
 export default nextConfig
